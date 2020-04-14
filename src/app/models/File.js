e5670e4ae8ba570ad0006c5a3ef422f2f6e7bc29
MayaraSs <may_stein@hotmail.com>
2020-04-14 06:10:44 -0300

    List of service providers

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          ger() {
+            return `htpp://localhost:3333/fils/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
